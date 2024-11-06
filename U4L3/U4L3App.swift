44abbd194da934e7da25df7ca0e6048d537bfb26
Dmitrii <dmitrii.nazarov7703@gmail.com>
2024-11-06 22:37:02 +0300

    Initial Commit

---
+++ U4L3/U4L3App.swift
@@ -0,0 +1,17 @@
+//
+//  U4L3App.swift
+//  U4L3
+//
+//  Created by Dmitrii Nazarov on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct U4L3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
